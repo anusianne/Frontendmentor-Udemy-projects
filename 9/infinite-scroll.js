8df795c3a052de25fa425d7a04874c5ec00957c5
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-04-29 23:40:28 +0200

    displaying photo data from unsplash api

--- 9/infinite-scroll.js
+++ 9/infinite-scroll.js
@@ -1,14 +1,41 @@
+const imgContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let photosArr = [];
 const count = 30;
 const apiKEY = `7x0Lp30rR3ej-06lEAGWg0wBIySB302W3WsSerf6N4w`;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKEY}&count=${count}`;
 
+function setAttributes(element, attr) {
+  for (const key in attr) {
+    element.setAttribute(key, attr[key]);
+  }
+}
+function displayPhotos() {
+  photosArr.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    item.appendChild(img);
+    imgContainer.appendChild(item);
+  });
+}
+
 async function getPhotos() {
   try {
     const res = await fetch(apiURL);
-    const data = res.json();
-    console.log(data);
+    photosArr = await res.json();
+    displayPhotos();
   } catch (e) {
     console.log(e);
   }
 }
+
 getPhotos();
