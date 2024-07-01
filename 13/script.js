b7cf297019e3b598c85f1aef9baf6ae9e4561635
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-01 16:46:26 +0200

    added playBtn and pauseBtn addEventListener

---
+++ 13/script.js
@@ -0,0 +1,20 @@
+const audio = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const playBtn = document.getElementById("play");
+
+let isPlaying = false;
+
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  audio.play();
+}
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  audio.pause();
+}
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
