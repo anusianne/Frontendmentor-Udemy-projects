afc69226771c8ffedd5fdd1ba01fbeed693e5a86
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-01 19:38:54 +0200

    added prevBtn and nextBtn functionality

--- 13/script.js
+++ 13/script.js
@@ -1,9 +1,34 @@
+const img = document.querySelector("img");
+const title = document.querySelector("title");
+const artist = document.getElementById("artist");
 const audio = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const playBtn = document.getElementById("play");
-
 let isPlaying = false;
+
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Metric",
+    artist: "Metric/Jacinto Design",
+  },
+];
 
 function playSong() {
   isPlaying = true;
@@ -18,3 +43,33 @@ function pauseSong() {
   audio.pause();
 }
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  audio.src = `music/${song.name}.mp3`;
+  img.src = `img/${song.name}.jpg`;
+  playSong();
+}
+let songIndex = 0;
+loadSong(songs[songIndex]);
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
