84a9a683799e0e9d121cff80685c553bd7ee7356
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2025-02-26 18:04:22 +0100

    real-time count completed

---
+++ 19/index.js
@@ -0,0 +1,16 @@
+const textarea = document.getElementById("textarea");
+const totalCounter = document.getElementById("total-counter");
+const remainingCounter = document.getElementById("remaining-counter");
+
+textarea.addEventListener("keyup", () => {
+  updateCounter();
+  updateRemaining();
+});
+
+function updateCounter() {
+  totalCounter.innerText = textarea.value.length;
+}
+function updateRemaining() {
+  remainingCounter.innerText =
+    textarea.getAttribute("maxlength") - textarea.value.length;
+}
