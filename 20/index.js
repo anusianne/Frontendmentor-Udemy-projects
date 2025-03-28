7dd26162e05c95eb8555ef91fea69802c91926c8
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2025-03-28 23:02:43 +0100

    added random color generator

---
+++ 20/index.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+const colorContainer = document.querySelectorAll(".color-container");
+
+colorContainer.forEach((el) => {
+  el.addEventListener("click", () => {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+    el.style.backgroundColor = randomColor;
+    console.log(randomColor);
+  });
+});
