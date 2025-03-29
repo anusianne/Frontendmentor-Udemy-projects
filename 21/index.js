1052fcd87ab10f44f84fd5379f91f09e2e683720
Anna Zielińska <ann.ka.zielinska@gmail.com>
2025-03-29 23:28:17 +0100

    rotating gallery completed

---
+++ 21/index.js
@@ -0,0 +1,16 @@
+const imageContainer = document.querySelector(".image-container");
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+let x = 0;
+prevEl.addEventListener("click", () => {
+  x = x + 45;
+  updateGallery();
+});
+nextEl.addEventListener("click", () => {
+  x = x - 45;
+  updateGallery();
+});
+
+function updateGallery() {
+  imageContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
